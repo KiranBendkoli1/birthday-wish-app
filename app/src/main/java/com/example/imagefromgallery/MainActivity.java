de9c804c095adea9b496ebfafcac6270e712a06d
KiranBendkoli1 <bendkolikiran@kbtcoe.org>
2021-04-27 16:38:38 +0530

    added music player in it Happy Birthday to You Ji

--- app/src/main/java/com/example/imagefromgallery/MainActivity.java
+++ app/src/main/java/com/example/imagefromgallery/MainActivity.java
@@ -3,22 +3,36 @@ package com.example.imagefromgallery;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Application;
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener  {
     private ImageView img;
     Uri imageUri;
+    private Button play,pause,stop;
+    MediaPlayer mediaPlayer;
+    int pauseCurrentPosition;
     private static final int PICK_IMAGE=1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        play = findViewById(R.id.play);
+        pause = findViewById(R.id.pause);
+        stop = findViewById(R.id.stop);
+
+        play.setOnClickListener(MainActivity.this);
+        pause.setOnClickListener(MainActivity.this);
+        stop.setOnClickListener(MainActivity.this);
 
         img = findViewById(R.id.imageView);
         img.setOnClickListener(new View.OnClickListener() {
@@ -46,5 +60,35 @@ public class MainActivity extends AppCompatActivity {
             }
         }
 
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.play:
+                if(mediaPlayer == null){
+                    mediaPlayer = mediaPlayer.create(getApplicationContext(),R.raw.hbddhanu);
+                    mediaPlayer.start();
+                }
+                else if(!mediaPlayer.isPlaying()){
+                    mediaPlayer.seekTo(pauseCurrentPosition);
+                    mediaPlayer.start();
+                }
+                break;
+            case R.id.pause:
+                if(mediaPlayer!=null){
+                    mediaPlayer.pause();
+                    pauseCurrentPosition = mediaPlayer.getCurrentPosition();
+                }
+                break;
+            case R.id.stop:
+                if(mediaPlayer != null){
+                    mediaPlayer.stop();
+                    mediaPlayer = null;
+                }
+                break;
+            default:
+                break;
+        }
     }
 }
